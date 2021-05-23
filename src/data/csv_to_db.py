e1aba7e66f481c9cba945a0a951d25a0c7c669fe
anggerenn <anggernolnem@gmail.com>
2021-05-24 02:27:58 +0700

    cleanup

--- src/data/csv_to_db.py
+++ src/data/csv_to_db.py
@@ -11,18 +11,15 @@ fn = './data/2020_Sales.csv'
 pathname = os.path.join(Path(__file__).resolve().parents[2],fn)
 df = pd.read_csv(pathname)
 
-print(f'two_up {pathname}')
-print(f'df {df.head(2)}')
+schema = os.environ['MYSQL_SCHEMA']
+user = os.environ['MYSQL_USER']
+pwd = up.quote_plus(os.environ['MYSQL_PASSWORD'])
+host = os.environ['MYSQL_HOST']
+db = os.environ['MYSQL_DB']
+url = f'{schema}://{user}:{pwd}@{host}/{db}'
 
-# schema = os.environ['MYSQL_SCHEMA']
-# user = os.environ['MYSQL_USER']
-# pwd = up.quote_plus(os.environ['MYSQL_PASSWORD'])
-# host = os.environ['MYSQL_HOST']
-# db = os.environ['MYSQL_DB']
-# url = f'{schema}://{user}:{pwd}@{host}/{db}'
+engine = create_engine(url)
 
-# engine = create_engine(url)
-
-# with engine.begin() as connection:
-#     for df in pd.read_csv(pathname, chunksize=1000):
-#         df.to_sql('pelir', con=connection, index=False, if_exists='append')
+with engine.begin() as connection:
+    for df in pd.read_csv(pathname, chunksize=1000):
+        df.to_sql('pelir', con=connection, index=False, if_exists='append')
